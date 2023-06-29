3fe74cff035f2bf14786828d5526c14ff070ae65
Makson Sillas <maksonsantoss4@gmail.com>
2023-06-29 10:07:54 -0300

    refactor: exclusão da rota de testar erro

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,6 @@
 import "express-async-errors";
 import express from "express";
 import { userRoutes } from "./routes/users.routes";
-import { routerError } from "./routes/teste.handleerror";
 import { handleError } from "./middlewares/handleError.middleware";
 import { walletsRoutes } from "./routes/wallets.routes";
 import { transactionRoutes } from "./routes/transaction.routes";
@@ -13,7 +12,6 @@ app.use(express.json());
 // routes
 app.use("/users", userRoutes);
 app.use("/wallets", walletsRoutes);
-app.use("/errortest", routerError);
 app.use("/transactions", transactionRoutes);
 
 // error handling middleware
--- src/routes/teste.handleerror.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-export const routerError = Router();
-
-routerError.get("", (req, res, next) => {
-  // Causando um erro intencionalmente
-  const error = new Error("Erro de exemplo");
-  next(error);
-});
